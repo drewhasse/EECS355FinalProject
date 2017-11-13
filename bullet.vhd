6ed12cd7ee3e99cdfec3bd22b5cb1be2ef490d60
Kushal Gourikrishna <kushalgourikrishna2018@u.northwestern.edu>
2017-11-13 00:33:08 -0600

    fixed semi colon issues

--- bullet.vhd
+++ bullet.vhd
@@ -6,11 +6,11 @@ entity bullet is
 generic (
 	w : std_logic_vector (3 downto 0);
 	h : std_logic_vector (3 downto 0);
-	rgb_color : std_logic_vector (9 downto 0);
+	rgb_color : std_logic_vector (9 downto 0)
 );
 port (
 	fire : in std_logic;
-	initial_x_pos : in std_logic_vector (9 downto 0)
+	initial_x_pos : in std_logic_vector (9 downto 0);
 	y_pos : out std_logic_vector (8 downto 0)
 );
 end entity bullet;
